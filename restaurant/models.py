d244303006105e79781e561adb55bce437e81d14
Muhammad Ahmad El-kufahn <muhammadelkufahn@gmail.com>
2023-07-19 22:38:09 +0100

    added djoser library and installed drf,modified views.py, urls.py and settings.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     No_of_guests = models.IntegerField()
@@ -11,5 +9,8 @@ class Booking(models.Model):
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+    
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView, ListAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, UserSerializer ,BookingSerializer
 # Create your views here.
 
+# def index(request):
+#     return render(request, 'index.html', {})
+class UserViews(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+    
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet (ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
